55a0a073ac91a38f859442878223d3ccb2d78be3
Rudy Xiong <xinjiguaike@163.com>
2014-05-17 16:58:10 +0800

    change tab to white space

--- DangDangAutoPal/MainWindow.xaml.cs
+++ DangDangAutoPal/MainWindow.xaml.cs
@@ -28,105 +28,105 @@ using DangDangAutoPal.Properties;
 
 namespace DangDangAutoPal
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window 
-	{
-		private AutoPal DangDangPal;
-		public MainWindow()
-		{
-			InitializeComponent();
-			DangDangPal = new AutoPal();
-			this.DataContext = DangDangPal;
-			pwdBoxADSL.Password = Settings.Default.ADSLPasswordSettings;
-		}
-		
-		private async void MainWindow_Closed(object sender, EventArgs e)
-		{
-			Settings.Default.ADSLPasswordSettings = pwdBoxADSL.Password;
-			Settings.Default.Save(); 
-			await CleanUpAsync();
-			Trace.TraceInformation("Rudy Trace, Application Exited.");
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window 
+    {
+        private AutoPal DangDangPal;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DangDangPal = new AutoPal();
+            this.DataContext = DangDangPal;
+            pwdBoxADSL.Password = Settings.Default.ADSLPasswordSettings;
+        }
+        
+        private async void MainWindow_Closed(object sender, EventArgs e)
+        {
+            Settings.Default.ADSLPasswordSettings = pwdBoxADSL.Password;
+            Settings.Default.Save(); 
+            await CleanUpAsync();
+            Trace.TraceInformation("Rudy Trace, Application Exited.");
+        }
 
-		
+        
 
-		private void OnBrowserQQ(object sender, RoutedEventArgs e)
-		{
-			OpenFileDialog dlg = new OpenFileDialog();
-			dlg.DefaultExt = ".xlsx";
-			dlg.Filter = "Excel(2007,2010)|*.xlsx";
+        private void OnBrowserQQ(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".xlsx";
+            dlg.Filter = "Excel(2007,2010)|*.xlsx";
 
-			Nullable<bool> result = dlg.ShowDialog();
-			if (result == true)
-				DangDangPal.QQAccountFile = dlg.FileName;
-		}
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+                DangDangPal.QQAccountFile = dlg.FileName;
+        }
 
-		private void OnBrowserTenPay(object sender, RoutedEventArgs e)
-		{
-			OpenFileDialog dlg = new OpenFileDialog();
-			dlg.DefaultExt = ".xlsx";
-			dlg.Filter = "Excel(2007,2010)|*.xlsx";
+        private void OnBrowserTenPay(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".xlsx";
+            dlg.Filter = "Excel(2007,2010)|*.xlsx";
 
-			Nullable<bool> result = dlg.ShowDialog();
-			if (result == true)
-				DangDangPal.TenpayAccountFile = dlg.FileName;
-		}
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+                DangDangPal.TenpayAccountFile = dlg.FileName;
+        }
 
-		private void OnPWDChanged(object sender, RoutedEventArgs e)
-		{
-			DangDangPal.ADSLPassword = pwdBoxADSL.Password;
-		}
+        private void OnPWDChanged(object sender, RoutedEventArgs e)
+        {
+            DangDangPal.ADSLPassword = pwdBoxADSL.Password;
+        }
 
-		private void OnStopPalling(object sender, RoutedEventArgs e)
-		{
-			Trace.TraceInformation("Rudy Trace, OnStopPalling: Pal Stopped.");
-			gdBeginPal.Visibility = Visibility.Visible;
-			gdPalling.Visibility = Visibility.Hidden;
-		}
+        private void OnStopPalling(object sender, RoutedEventArgs e)
+        {
+            Trace.TraceInformation("Rudy Trace, OnStopPalling: Pal Stopped.");
+            gdBeginPal.Visibility = Visibility.Visible;
+            gdPalling.Visibility = Visibility.Hidden;
+        }
 
-		private void OnBrowserBindQQAccount(object sender, RoutedEventArgs e)
-		{
-			OpenFileDialog dlg = new OpenFileDialog();
-			dlg.DefaultExt = ".xlsx";
-			dlg.Filter = "Excel(2007,2010)|*.xlsx";
+        private void OnBrowserBindQQAccount(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".xlsx";
+            dlg.Filter = "Excel(2007,2010)|*.xlsx";
 
-			Nullable<bool> result = dlg.ShowDialog();
-			if (result == true)
-				DangDangPal.BindQQAccountFile = dlg.FileName;
-		}
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+                DangDangPal.BindQQAccountFile = dlg.FileName;
+        }
 
-		private async void OnBeginBind(object sender, RoutedEventArgs e)
-		{
-			await PrepareEnvironmentAsync("Chrome");
+        private async void OnBeginBind(object sender, RoutedEventArgs e)
+        {
+            await PrepareEnvironmentAsync("Chrome");
 
-			bool bRet = DangDangPal.SetWebDriver("Chrome");
-			if (bRet)
-				await DangDangPal.BindAllAccountAddressAsync();
-			else
-				Trace.TraceInformation("Rudy Trace, OnBeginBind: Set Web Driver Failed.");
-		}
+            bool bRet = DangDangPal.SetWebDriver("Chrome");
+            if (bRet)
+                await DangDangPal.BindAllAccountAddressAsync();
+            else
+                Trace.TraceInformation("Rudy Trace, OnBeginBind: Set Web Driver Failed.");
+        }
 
-		private async void OnBeginPal(object sender, RoutedEventArgs e)
-		{
-			Trace.WriteLine("Rudy Trace, OnBeginPal: Begin Pal! ");
-			gdBeginPal.Visibility = Visibility.Hidden;
-			gdPalling.Visibility = Visibility.Visible;
+        private async void OnBeginPal(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("Rudy Trace, OnBeginPal: Begin Pal! ");
+            gdBeginPal.Visibility = Visibility.Hidden;
+            gdPalling.Visibility = Visibility.Visible;
 
-			await PrepareEnvironmentAsync("Chrome");
+            await PrepareEnvironmentAsync("Chrome");
 
-			string ProductLink = "http://product.dangdang.com/1263628906.html";
-			bool bRet = DangDangPal.SetWebDriver("Chrome");
-			if (bRet)
+            string ProductLink = "http://product.dangdang.com/1263628906.html";
+            bool bRet = DangDangPal.SetWebDriver("Chrome");
+            if (bRet)
                 await DangDangPal.AutoPalProcessAsync("672045573", "mbi@88820", ProductLink);
-			else
-				Trace.WriteLine("Rudy Trace, OnBeginPal: Set Web Driver Failed.");
-		}
+            else
+                Trace.WriteLine("Rudy Trace, OnBeginPal: Set Web Driver Failed.");
+        }
 
-		private Task PrepareEnvironmentAsync(string BrowserType)
-		{
-			Trace.WriteLine("Rudy Trace, PrepareEnvironmentAsync: Begin Prepare Environment！");
+        private Task PrepareEnvironmentAsync(string BrowserType)
+        {
+            Trace.WriteLine("Rudy Trace, PrepareEnvironmentAsync: Begin Prepare Environment！");
             return Task.Run(() =>
             {
                 if (BrowserType.Equals("Chrome"))
@@ -143,15 +143,15 @@ namespace DangDangAutoPal
                     }
                 }
             });
-		}
+        }
 
-		private async Task CleanUpAsync()
-		{
-			Trace.WriteLine("Rudy Trace, Cleaning up the Environment...");
+        private async Task CleanUpAsync()
+        {
+            Trace.WriteLine("Rudy Trace, Cleaning up the Environment...");
             if (DangDangPal != null)
                 await DangDangPal.CleanUpAsync().ConfigureAwait(false);
             Trace.WriteLine("Rudy Trace, Clea up done.");
-		}
+        }
 
         private async void OnTest_Click(object sender, RoutedEventArgs e)
         {
@@ -162,5 +162,5 @@ namespace DangDangAutoPal
             else
                 Trace.WriteLine("Rudy Trace, OnTest_Click: Set Web Driver Failed.");
         }
-	}
+    }
 }
--- DangDangAutoPal/Models/Globals.cs
+++ DangDangAutoPal/Models/Globals.cs
@@ -6,81 +6,81 @@ using System.Threading.Tasks;
 
 namespace DangDangAutoPal.Models
 {
-	public class Globals
-	{
-		public const string CHROME_DRIVER_TITLE = "C:\\Windows\\system32\\chromedriver.exe";
-		public const string IE_DRIVER_TITLE = "C:\\Windows\\system32\\internetexplorerdriver.exe";
+    public class Globals
+    {
+        public const string CHROME_DRIVER_TITLE = "C:\\Windows\\system32\\chromedriver.exe";
+        public const string IE_DRIVER_TITLE = "C:\\Windows\\system32\\internetexplorerdriver.exe";
 
-		#region======================Page Title======================
-		public const string LOGGEDIN_TITLE = "当当网—网上购物中心";
-		public const string PAYMENT_TITLE = "当当网--支付中心";
-		public const string TENPAY_TITLE = "财付通 - 支付中心";
+        #region======================Page Title======================
+        public const string LOGGEDIN_TITLE = "当当网—网上购物中心";
+        public const string PAYMENT_TITLE = "当当网--支付中心";
+        public const string TENPAY_TITLE = "财付通 - 支付中心";
         public const string LOGIN_PAGE_TITLE = "登录 - 当当网";
-		#endregion
+        #endregion
 
-		#region======================Page Url========================
-		public const string LOGIN_URL = "https://login.dangdang.com/signin.aspx";
-		public const string ONEKEY_BUY_URL = "http://customer.dangdang.com/onekey_buy/info.php";
-		#endregion
+        #region======================Page Url========================
+        public const string LOGIN_URL = "https://login.dangdang.com/signin.aspx";
+        public const string ONEKEY_BUY_URL = "http://customer.dangdang.com/onekey_buy/info.php";
+        #endregion
 
-		#region======================Page Elements===================
-		//Log in information
-		public const string LOGIN_LINK_CLASS = "login_link";
-		public const string QQ_LOGIN_XPATH = "//a[@title='QQ']";
-		public const string QQ_FRAME_NAME = "ptlogin_iframe";
-		public const string QQ_ACCOUNT_ID = "u";
-		public const string QQ_PASSWORD_ID = "p";
-		public const string QQ_LOGINBTN_ID = "login_button";
-		public const string QQ_SWITCH_ACCOUNT_LOGIN_ID = "switcher_plogin";
+        #region======================Page Elements===================
+        //Log in information
+        public const string LOGIN_LINK_CLASS = "login_link";
+        public const string QQ_LOGIN_XPATH = "//a[@title='QQ']";
+        public const string QQ_FRAME_NAME = "ptlogin_iframe";
+        public const string QQ_ACCOUNT_ID = "u";
+        public const string QQ_PASSWORD_ID = "p";
+        public const string QQ_LOGINBTN_ID = "login_button";
+        public const string QQ_SWITCH_ACCOUNT_LOGIN_ID = "switcher_plogin";
 
-		public const string BTN_ADD_ADDRESS_CLASS = "add_address_btn";
+        public const string BTN_ADD_ADDRESS_CLASS = "add_address_btn";
 
-		//Delivery Address Infomation
-		public const string SHIP_MAN_ID = "ship_man";
-		public const string SELECT_COUNTRY_ID = "country_id";
-		public const string SELECT_PROVINCE_ID = "province_id";
-		public const string SELECT_CITY_ID = "city_id";
-		public const string SELECT_TOWN_ID = "town_id";
-		public const string ADDRESS_DETAIL_ID = "addr_detail";
-		public const string ZIP_CODE_ID = "ship_zip";
-		public const string MOBILE_ID = "ship_mb";
-		public const string CONFIRM_ADDRESS_XPATH = "//a[@href='javascript:show_shipment();']";
-		public const string RADIO_NORMALSHIP_XPATH = "//input[@name='ship_type' and @value='1']";
-		public const string CONFIRM_PAYMENT_XPATH = "//a[@href='javascript:show_payment();']";
+        //Delivery Address Infomation
+        public const string SHIP_MAN_ID = "ship_man";
+        public const string SELECT_COUNTRY_ID = "country_id";
+        public const string SELECT_PROVINCE_ID = "province_id";
+        public const string SELECT_CITY_ID = "city_id";
+        public const string SELECT_TOWN_ID = "town_id";
+        public const string ADDRESS_DETAIL_ID = "addr_detail";
+        public const string ZIP_CODE_ID = "ship_zip";
+        public const string MOBILE_ID = "ship_mb";
+        public const string CONFIRM_ADDRESS_XPATH = "//a[@href='javascript:show_shipment();']";
+        public const string RADIO_NORMALSHIP_XPATH = "//input[@name='ship_type' and @value='1']";
+        public const string CONFIRM_PAYMENT_XPATH = "//a[@href='javascript:show_payment();']";
 
-		public const string RADIO_NETPAY_XPATH = "//input[@name='pay_id' and @value='-1']";
-		public const string CONFIRM_INVOICE_XPATH = "//a[@href='javascript:show_invoice();']";
+        public const string RADIO_NETPAY_XPATH = "//input[@name='pay_id' and @value='-1']";
+        public const string CONFIRM_INVOICE_XPATH = "//a[@href='javascript:show_invoice();']";
 
-		public const string CHB_INVOICE_ID = "no_need_invoice";
-		public const string CHECK_SUBMIT_XPATH = "//a[@onclick='javascript:check_submit();return false;']";
+        public const string CHB_INVOICE_ID = "no_need_invoice";
+        public const string CHECK_SUBMIT_XPATH = "//a[@onclick='javascript:check_submit();return false;']";
 
-		//Product Page
-		public const string COLOR_IMAGE1_XPATH = "//a[@id='cl_0']/img";
-		public const string COLOR_IMAGE2_XPATH = "//a[@id='cl_1']/img";
-		public const string COLOR_IMAGE3_XPATH = "//a[@id='cl_2']/img";
-		public const string COLOR_IMAGE4_XPATH = "//a[@id='cl_3']/img";
-		public const string BUY_NUM_ID = "buy_num";
-		public const string BUY_NOW_ID = "buy_now_button";
-		public const string ADD_TO_CART_ID = "part_buy_button";
-		public const string BUY_NOW_XPATH = "//div[@class='btn_p']/a[@id='buy_now_button']";
-		public const string BUY_NOW_POPUP_ID = "div_onekey_select_pop";
-		public const string BTN_CONFIRM_BUY_ID = "onekey_select_pop_confirm";
+        //Product Page
+        public const string COLOR_IMAGE1_XPATH = "//a[@id='cl_0']/img";
+        public const string COLOR_IMAGE2_XPATH = "//a[@id='cl_1']/img";
+        public const string COLOR_IMAGE3_XPATH = "//a[@id='cl_2']/img";
+        public const string COLOR_IMAGE4_XPATH = "//a[@id='cl_3']/img";
+        public const string BUY_NUM_ID = "buy_num";
+        public const string BUY_NOW_ID = "buy_now_button";
+        public const string ADD_TO_CART_ID = "part_buy_button";
+        public const string BUY_NOW_XPATH = "//div[@class='btn_p']/a[@id='buy_now_button']";
+        public const string BUY_NOW_POPUP_ID = "div_onekey_select_pop";
+        public const string BTN_CONFIRM_BUY_ID = "onekey_select_pop_confirm";
 
-		//Payment page
-		public const string TAB_PAYMENT_PLATFORM_ID = "go_tab3";
-		public const string RADIO_TENPAY_XPATH = "//p[@bid='44']/input";
-		public const string BTN_NEXT_ID = "A4";
+        //Payment page
+        public const string TAB_PAYMENT_PLATFORM_ID = "go_tab3";
+        public const string RADIO_TENPAY_XPATH = "//p[@bid='44']/input";
+        public const string BTN_NEXT_ID = "A4";
 
-		//Tenpay Login Page
-		public const string TENPAY_LOGIN_ERR_MSG_ID = "login_err_msg";
-		public const string TENPAY_USERNAME_ID = "login_uin";
-		public const string TENPAY_PASSWORD_ID = "login_pwd";
-		public const string TENPAY_LOGIN_ID = "btn_login";
+        //Tenpay Login Page
+        public const string TENPAY_LOGIN_ERR_MSG_ID = "login_err_msg";
+        public const string TENPAY_USERNAME_ID = "login_uin";
+        public const string TENPAY_PASSWORD_ID = "login_pwd";
+        public const string TENPAY_LOGIN_ID = "btn_login";
 
-		//Tenpay Payment Page
-		public const string RADIO_BALANCEPAY_CLASS = "ctrl-radio";
-		public const string CTRL_TENPWD_LABEL_XPATH = "//div[@id='paypwd_line']/label";
-		public const string CONFIRM_TO_PAY_ID = "btn_pay_submit";
-		#endregion
-	}
+        //Tenpay Payment Page
+        public const string RADIO_BALANCEPAY_CLASS = "ctrl-radio";
+        public const string CTRL_TENPWD_LABEL_XPATH = "//div[@id='paypwd_line']/label";
+        public const string CONFIRM_TO_PAY_ID = "btn_pay_submit";
+        #endregion
+    }
 }
