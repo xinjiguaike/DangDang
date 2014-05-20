c221a4d8da97066a2df06d7f01c0df431d04357b
Rudy Xiong <xinjiguaike@163.com>
2014-05-21 01:26:36 +0800

    MouseClick

--- DangDangAutoPal/App.xaml.cs
+++ DangDangAutoPal/App.xaml.cs
@@ -10,37 +10,77 @@ using OpenQA.Selenium;
 
 namespace DangDangAutoPal
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-		[DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
-		private static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
-		[DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
-		private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-		[DllImport("user32.dll", CharSet = CharSet.Auto)]
-		private static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
-		/// <summary>
-		/// Hiding Window
-		/// </summary>
-		/// <param name="consoleTitle">Title of the Window</param>
-		public static void WindowHide(string consoleTitle)
-		{
-			IntPtr a = FindWindow("ConsoleWindowClass", consoleTitle);
-			if (a != IntPtr.Zero)
-				ShowWindow(a, 0);//hide the window
-			else
-				throw new Exception("can't hide console window");
-		}
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
+        [DllImport("User32")]
+        public extern static void mouse_event(int dwFlags, int dx, int dy, int dwData, IntPtr dwExtraInfo);
 
-		public static void KillExcel(Microsoft.Office.Interop.Excel.Application excel)
-		{
-			IntPtr t = new IntPtr(excel.Hwnd);
-			int k = 0;
-			GetWindowThreadProcessId(t, out k);
-			System.Diagnostics.Process p = System.Diagnostics.Process.GetProcessById(k);
-			p.Kill();
-		}
-	}
+        [DllImport("User32")]
+        public extern static void SetCursorPos(int x, int y);
+
+        [DllImport("User32")]
+        public extern static bool GetCursorPos(out System.Drawing.Point p);
+
+
+        public enum MouseEventFlags
+        {
+            Move = 0x0001,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            Wheel = 0x0800,
+            Absolute = 0x8000
+        }
+
+        public static void ClickMouseLeft(int x, int y)
+        {
+            System.Drawing.Point p = new System.Drawing.Point();
+            GetCursorPos(out p);
+            try
+            {
+                SetCursorPos(x, y);
+                mouse_event((int)(MouseEventFlags.LeftDown | MouseEventFlags.Absolute), 0, 0, 0, IntPtr.Zero);
+                mouse_event((int)(MouseEventFlags.LeftUp | MouseEventFlags.Absolute), 0, 0, 0, IntPtr.Zero);
+            }
+            finally
+            {
+                SetCursorPos(p.X, p.Y);
+            }
+        }
+
+
+        /// <summary>
+        /// Hiding Window
+        /// </summary>
+        /// <param name="consoleTitle">Title of the Window</param>
+        public static void WindowHide(string consoleTitle)
+        {
+            IntPtr a = FindWindow("ConsoleWindowClass", consoleTitle);
+            if (a != IntPtr.Zero)
+                ShowWindow(a, 0);//hide the window
+            else
+                throw new Exception("can't hide console window");
+        }
+
+        public static void KillExcel(Microsoft.Office.Interop.Excel.Application excel)
+        {
+            IntPtr t = new IntPtr(excel.Hwnd);
+            int k = 0;
+            GetWindowThreadProcessId(t, out k);
+            System.Diagnostics.Process p = System.Diagnostics.Process.GetProcessById(k);
+            p.Kill();
+        }
+    }
 }
